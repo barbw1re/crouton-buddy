99f4705778bd61a0c3b25b452185a1f8110b786b
Kris Johnson <kris@kdjonline.com>
2018-07-24 13:30:53 +1000

    Cleanup and formatting cleanup of installer script

--- crouton-buddy.sh
+++ crouton-buddy.sh
@@ -1,9 +1,22 @@
 #!/bin/bash
 
+# Ensure we are running under bash (as this will not work under sh or dash etc)
+if [ "$BASH_SOURCE" = "" ]; then
+    if [ ! -x /bin/bash ]; then
+        echo "ERROR: $0 needs to be run by bash and /bin/bash does not appear to be available."
+        exit 1
+    fi
+    /bin/bash "$0"
+    exit 0
+fi
+
+#
+# Helper to clearly display an error message and exit
+#
 errorExit() {
     echo -e "\n****************\n"
     echo "ERROR: $1"
-    if [ ! -z "$2" ]; then
+    if [[ ! -z "$2" ]]; then
         shift
         echo ""
         for msg in "$@" ; do
@@ -15,28 +28,19 @@ errorExit() {
     exit 1    
 }
 
-# Ensure we are running under bash (will not work under sh or dash etc)
-if [ "$BASH_SOURCE" = "" ]; then
-    [ -x /bin/bash ] || errorExit "$0 needs to be run by bash and /bin/bash does not appear to be available."
-    /bin/bash "$0"
-    exit 0 
-fi
-
 # Ensure we are running as root
-if [[ `id -u` -gt 0 ]]; then
-    errorExit "Unable to run as regular user. Please run as root:" \
-              "  $ sudo sh $0"
-fi
+(( `id -u` )) && errorExit "Unable to run as regular user. Please run as root:" \
+                           "  $ sudo sh $0"
 
 # Get script file and run directory
 me=$(basename "$0")
-dir=`pwd`
+execDir=`pwd`
 cd $(dirname "$0")
 bin=`pwd`
-cd "$dir"
+cd "$execDir"
 
 # Determine whether we are running inside or outside of a chroot
-if [[ "`ls -id / | awk '{print $1}'`" -eq 2 ]]; then
+if [[ `ls -id / | cut -d ' ' -f1` -eq 2 ]]; then
     IN_CHROOT=0
     HOME_DIR="/home/chronos/user"
 else
@@ -47,12 +51,6 @@ fi
 # Set applicable path to Downloads directory
 ROOT_DIR="$HOME_DIR/Downloads"
 
-# Crouton Buddy Paths
-CB_URL="https://raw.githubusercontent.com/barbw1re/crouton-buddy/assets/crouton-buddy.tar.gz"
-CB_ROOT="$ROOT_DIR/crouton-buddy"
-[[ $IN_CHROOT -eq 1 ]] && CB_APP="$CB_ROOT/cb-guest.sh" || CB_APP="$CB_ROOT/cb-host.sh"
-CB_ZIP="$ROOT_DIR/crouton-buddy.tar.gz"
-
 # Ensure we are running from Downloads directory
 if [[ "$bin" != "$ROOT_DIR" ]]; then
     errorExit "Running script from invalid directory." \
@@ -60,15 +58,22 @@ if [[ "$bin" != "$ROOT_DIR" ]]; then
               "You seem to be running this from $bin not $ROOT_DIR"
 fi
 
+# Crouton Buddy Urls and Paths
+CB_ZIP="$ROOT_DIR/crouton-buddy.tar.gz"
+CB_URL="https://raw.githubusercontent.com/barbw1re/crouton-buddy/assets/crouton-buddy.tar.gz"
+
+CB_ROOT="$ROOT_DIR/crouton-buddy"
+(( $IN_CHROOT )) && CB_APP="$CB_ROOT/cb-guest.sh" || CB_APP="$CB_ROOT/cb-host.sh"
+
 # Download latest Crouton Buddy scripts
 cbInstall() {
     # Download scripts bundle
     curl $CB_URL -L -o $CB_ZIP
-    [[ $? -eq 0 ]] || errorExit "Unable to download application package."
+    (( $? )) && errorExit "Unable to download application package."
 
     # Extract scripts
     tar -zxf $CB_ZIP -C $ROOT_DIR
-    [[ $? -eq 0 ]] || errorExit "Unable to extract application from package."
+    (( $? )) && errorExit "Unable to extract application from package."
 
     # Clean up
     [[ -f "$CB_ZIP" ]] && rm "$CB_ZIP"
@@ -82,7 +87,7 @@ fi
 # Verify application scripts were installed
 [[ -d "$CB_ROOT" && -s "$CB_APP" ]] || errorExit "Unable to download application dependencies."
 
-# Finally run the applicable main application script
+# Finally run the main menu script for our identified environment
 . $CB_APP
 cbRun
 
